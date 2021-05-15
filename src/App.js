a13af812bbb304140dde7326e91024078f26758d
alescofet <alex.escofet96@gmail.com>
2021-05-15 14:44:04 +0200

    client done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import socketIOClient from "socket.io-client";
+import SocketClient from "./components/SocketClient"
+const ENDPOINT = "http://127.0.0.1:4001";
 
 function App() {
+  const [loadClient, setLoadClient] = useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* LOAD OR UNLOAD THE CLIENT */}
+      <button onClick={() => setLoadClient(prevState => !prevState)}>
+        STOP CLIENT
+      </button>
+      {/* SOCKET IO CLIENT*/}
+      {loadClient ? <SocketClient /> : null}
+    </>
   );
 }
 
